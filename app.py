95a2e23addacc94578257b489c02be6ea2252e6d
Koonie19 <koonie2019@gmail.com>
2026-02-22 17:02:32 +0000

    Initial commit (removed secrets)

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from pymongo import MongoClient
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+mongo_uri = os.getenv("MONGODB_URI")
+
+client = MongoClient(mongo_uri)
+db = client ["chatbot"]
+collection = db["users"]
+
+app = FastAPI()
+
+class chatrequest(BaseModel):
+    user_id: str
+    question: str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+prompt = ChatPromptTemplate.from_messages(
+[
+    ("system", "You are a fitness coach, tell me the answer with respect to fitness things"),
+    ("placeholder","{history}"),
+    ("user", "{question}")
+]
+)
+
+llm = ChatGroq(api_key = groq_api_key, model="openai/gpt-oss-20b")
+chain = prompt | llm
+
+user_id = "user3"
+
+def get_history(user_id):
+    chats = collection.find({"user_id": user_id}).sort("timestamp", 1)
+    history = []
+
+    for chat in chats:
+        history.append((chat["role"], chat["message"]))
+    return history
+@app.get("/")
+def home():
+    return {"message": "Welcome to Fitness Coach Chatbot API"}
+
+@app.post("/chat")
+def chat(request: chatrequest):
+    history = get_history(request.user_id)
+    response = chain.invoke({"history": history, "question": request.question})
+
+    collection.insert_one({
+    "user_id": request.user_id,
+    "role": "user",
+    "message": request.question,
+    "timestamp": datetime.utcnow()
+})
+    collection.insert_one({
+    "user_id": request.user_id,
+    "role": "assistant",
+    "message": response.content,
+    "timestamp": datetime.utcnow()
+})
+
+    return {"response": response.content}
